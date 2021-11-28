860480eb43edc25e888b43091495a0a83c88e5b2
namnt11607 <namntph11607@fpt.edu.com>
2021-11-28 22:22:26 +0700

    update namnt 28/11

--- src/main/java/poly/datn/rest/controller/BookingRestController.java
+++ src/main/java/poly/datn/rest/controller/BookingRestController.java
@@ -3,6 +3,7 @@ package poly.datn.rest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class BookingRestController {
 
 	@GetMapping("")
 	public List<Booking> getAll(){
-		return bookingService.findAll();
+		return bookingService.findAll(Sort.by(Sort.Direction.DESC, "createDate"));
 	}
 
 	@GetMapping("/WFC")
