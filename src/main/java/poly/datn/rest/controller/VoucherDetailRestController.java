14acfdd3d958b811191f2eaca86c7a18345149b0
namnt11607 <namntph11607@fpt.edu.com>
2021-12-02 03:13:26 +0700

    update customer, GiftVoucher

--- src/main/java/poly/datn/rest/controller/VoucherDetailRestController.java
+++ src/main/java/poly/datn/rest/controller/VoucherDetailRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import poly.datn.entity.Voucher;
@@ -19,6 +20,12 @@ public class VoucherDetailRestController {
 	@Autowired
 	VoucherDetailService voucherDetailService;
 	
+	@GetMapping("/rest/voucherDetail")
+	public List<Voucherdetail> getAllVoucherDetail(){
+
+		return voucherDetailService.findAll();
+	}
+	
 	@GetMapping("/rest/voucherdetailByCustomer/{id}")
 	public List<Voucher> getvoucherByCus(@PathVariable("id") Integer id){
 
@@ -29,5 +36,9 @@ public class VoucherDetailRestController {
 	public VoucherDetailInfoDTO PayBooking(VoucherDetailInfoDTO voucherDetailInfoDTO){
 		return voucherDetailService.completeBooking(voucherDetailInfoDTO);
 	}
-
+	
+	@PostMapping("/rest/voucherDetail")
+	public Voucherdetail saveVoucherDetail(@RequestBody Voucherdetail voucherDetail){
+		return voucherDetailService.save(voucherDetail);
+	}
 }
