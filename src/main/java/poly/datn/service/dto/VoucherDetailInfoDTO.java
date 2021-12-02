07734b1ff5e30d80b12c37b0a20b84322b538826
Hai <hainv578@gmail.com>
2021-12-02 09:47:10 +0700

    haiDev

--- src/main/java/poly/datn/service/dto/VoucherDetailInfoDTO.java
+++ src/main/java/poly/datn/service/dto/VoucherDetailInfoDTO.java
@@ -1,18 +1,8 @@
 package poly.datn.service.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import poly.datn.entity.Booking;
-import poly.datn.entity.Customer;
-import poly.datn.entity.Voucher;
-
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
