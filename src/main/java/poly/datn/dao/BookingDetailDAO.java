d2287dd5f17d12850971adcce03ecc36da319ebf
namnt11607 <namntph11607@fpt.edu.com>
2021-12-03 22:31:03 +0700

    xoa file bookingDetailPK

--- src/main/java/poly/datn/dao/BookingDetailDAO.java
+++ src/main/java/poly/datn/dao/BookingDetailDAO.java
@@ -4,12 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.jpa.repository.Query;
 import poly.datn.entity.BookingDetail;
-import poly.datn.entity.BookingDetailPK;
 
 import javax.persistence.Tuple;
 import java.util.List;
 
-public interface BookingDetailDAO extends JpaRepository<BookingDetail, BookingDetailPK>{
+public interface BookingDetailDAO extends JpaRepository<BookingDetail, Integer>{
     @Query(value = "SELECT s.service.serviceName,s.service.image FROM BookingDetail s WHERE s.booking.id =?1")
     List<Tuple> selectServiceByIdBooking(Integer id);
 
