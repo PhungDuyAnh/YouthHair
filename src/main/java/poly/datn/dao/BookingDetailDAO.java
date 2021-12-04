0b247de1bcd40bb2135d6b2fe466afb01242ed5e
namnt11607 <namntph11607@fpt.edu.com>
2021-12-05 00:19:46 +0700

    them lai BookingDetailPK.java

--- src/main/java/poly/datn/dao/BookingDetailDAO.java
+++ src/main/java/poly/datn/dao/BookingDetailDAO.java
@@ -4,11 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.jpa.repository.Query;
 import poly.datn.entity.BookingDetail;
+import poly.datn.entity.BookingDetailPK;
 
 import javax.persistence.Tuple;
 import java.util.List;
 
-public interface BookingDetailDAO extends JpaRepository<BookingDetail, Integer>{
+public interface BookingDetailDAO extends JpaRepository<BookingDetail, BookingDetailPK>{
     @Query(value = "SELECT s.service.serviceName,s.service.image FROM BookingDetail s WHERE s.booking.id =?1")
     List<Tuple> selectServiceByIdBooking(Integer id);
 
--- src/main/java/poly/datn/rest/controller/ContactRestController.java
+++ src/main/java/poly/datn/rest/controller/ContactRestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -58,5 +59,6 @@ public class ContactRestController {
 		public List<Contact> getContactsCXL(@Param("keyword") String keyword){
 			return contactService.findContactStatus(false, keyword);
 		}
+		
 
 }
